8d30ed2ad9d11f0a27a41ade6b860af767973887
Md-Rifayet <rifayetkhanprince30@gmail.com>
2023-02-27 22:05:32 +0600

    first commit

---
+++ js/apps.js
@@ -0,0 +1,26 @@
+const API_KEY = `2c13c14b2ced83b37c8bd0d242176c1e`
+
+const searchTemperature = () =>{
+    const city = document.getElementById("city-name").value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    // console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data))
+}
+
+const setInnerText =(id, text) =>{
+    document.getElementById(id).innerHTML = text
+}
+
+const displayTemperature = (temperature) =>{
+    setInnerText('city', temperature.name)
+    setInnerText('temp', temperature.main.temp)
+    setInnerText('weather', temperature.weather[0].description)
+    // set weather url
+    const iconURL = `https://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', iconURL);
+    1
+    console.log(temperature)
+}
